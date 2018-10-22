c07589990c990efbf16670e6a9b72f921d71dcf5
Laura Klinger <laura.klinger@hotmail.fr>
2018-10-22 14:14:18 +0200

    Ajouts

--- application/views/view_Interface.php
+++ application/views/view_Interface.php
@@ -3,7 +3,6 @@
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Devoir Techdays</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <script src="<?php echo base_url(); ?>JQuery/jquery-3.1.1.js"></script>
     <script src="<?php echo base_url(); ?>JS/fonctions.js"></script>
